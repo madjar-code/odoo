cb56e2f2f15eedf866bf35ffe0c6dcb770c61037
evanmadjar@gmail.com <dmitrii@oneinvestmentholding.com>
2023-07-24 17:08:02 +0300

    feat: Create a new module

---
+++ custom_addons/awesome_tshirt/static/src/dashboard.js
@@ -0,0 +1,9 @@
+import { Component } from "@odoo/owl";
+import { registry } from "@web/core/registry";
+
+class AwesomeDashboard extends Component {}
+
+AwesomeDashboard.components = {};
+AwesomeDashboard.template = "awesome_tshirt.clientaction";
+
+registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);
