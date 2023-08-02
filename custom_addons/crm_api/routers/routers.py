c34499bcfe96e5536cebae7931314a6b811887c5
Evan Madjar <evan.madjar@datadrivemd.com>
2023-08-02 16:48:48 +0300

    feat: Add table routing to form tables

--- custom_addons/crm_api/routers/routers.py
+++ custom_addons/crm_api/routers/routers.py
@@ -1,21 +1,23 @@
+from enum import Enum
 from typing import Annotated
 from fastapi import (
     APIRouter,
     Depends,
     HTTPException,
-    Body,
 )
-from fastapi.openapi.models import Example
 from odoo.api import Environment
-# from odoo.addons.crm.models.crm_lead import Lead
 from odoo.addons.fastapi.dependencies import odoo_env
 from ..schemas import (
-    Lead,
-    LeadFormSchema
+    LeadFormSchema,
 )
 
 
 router = APIRouter(tags=['crm_lead'])
+
+
+TABLE_MAPPING = {
+    'SteponeForm': 'stepone.form',
+}
 
 
 @router.post('/crm_lead/{form_name}/')
@@ -24,16 +26,20 @@ async def create_lead(
         lead_form: LeadFormSchema,
         env: Annotated[Environment, Depends(odoo_env)],
     ):
+    try:
+        form_table_name = TABLE_MAPPING[form_name]
+        form_table = env[form_table_name]
+    except KeyError:
+        raise HTTPException(status_code=403, detail='Incorrect form name')
     lead_form_data = lead_form.model_dump(by_alias=True)
     crm_lead = env['crm.lead']
-    stepone_form = env['stepone.form']
     try:
-        with env.cr.savepoint():    
+        with env.cr.savepoint():
             lead_obj = crm_lead.sudo().create(lead_form_data['lead'])
             form_data = lead_form_data['form']
             form_data['lead_id'] = lead_obj.id
-            stepone_form.sudo().create(form_data)
+            form_table.sudo().create(form_data)
     except Exception as e:
-        error_messages = 'Something went wrong'
-        raise HTTPException(status_code=422, detail=error_messages)
+        print(e)
+        raise HTTPException(status_code=422, detail='Something went wrong')
     return {'message': 'OK!'}
