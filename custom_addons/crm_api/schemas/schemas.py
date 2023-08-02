17bc52102d8e4cb656fb45715063b006a092b4c1
Evan Madjar <evan.madjar@datadrivemd.com>
2023-08-02 12:53:17 +0300

    feat: Update crm_api schemas.py

--- custom_addons/crm_api/schemas/schemas.py
+++ custom_addons/crm_api/schemas/schemas.py
@@ -20,12 +20,12 @@ class Lead(BaseModel):
     website: HttpUrl
     email_from: EmailStr
     phone: constr(min_length=10, max_length=15)
-    company_id: int
-    user_id: int
+    company_id: Optional[int]
+    user_id: Optional[int]
     country_id: Optional[int]
     team_id: Optional[int]
     city: str
-    form: PropertyForm
+    # form: PropertyForm
 
-    class Config:
-        from_attributes = True
+    # class Config:
+    #     from_attributes = True
