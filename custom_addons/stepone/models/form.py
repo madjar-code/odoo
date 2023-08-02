b31cb9a8724a48c1e1bf110c44109d48bce170d6
Evan Madjar <evan.madjar@datadrivemd.com>
2023-08-02 15:45:13 +0300

    feat: Update SteponeForm with relation file

--- custom_addons/stepone/models/form.py
+++ custom_addons/stepone/models/form.py
@@ -5,6 +5,15 @@ class Form(models.Model):
     _name = 'stepone.form'
     _description = 'Form Model'
 
+    lead_id = fields.Many2one('crm.lead',
+                              inverse_name='form_id',
+                              string='Related Lead',
+                              ondelete='cascade')
     linkedIn = fields.Char(string='LinkedIn URL')
     file = fields.Char(string='File URL')
     introduction = fields.Text(string='Introduction')
+
+    _sql_constraints = (
+        ('lead_id_unique', 'UNIQUE(lead_id)',
+         'Each Form can be linked to only one CRM Lead.'),
+    )
