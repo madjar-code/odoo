98497d69d95d5827edee2b614e12779fcbf14490
Evan Madjar <evan.madjar@datadrivemd.com>
2023-08-02 13:00:52 +0300

    feat: Init stepone module

---
+++ custom_addons/stepone/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'stepone data',
+    'summary': """stepone website data with models and schema""",
+    'author': 'Evan Madjar',
+    'installable': True,
+    'application': True,
+}
