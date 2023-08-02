9fae6ae6005a71b80d6e40b2cdbe6c41eed1a0a4
Evan Madjar <evan.madjar@datadrivemd.com>
2023-08-02 15:47:13 +0300

    feat: Update lead-form endpoint

--- custom_addons/crm_api/routers/routers.py
+++ custom_addons/crm_api/routers/routers.py
@@ -2,20 +2,38 @@ from typing import Annotated
 from fastapi import (
     APIRouter,
     Depends,
+    HTTPException,
+    Body,
 )
+from fastapi.openapi.models import Example
 from odoo.api import Environment
 # from odoo.addons.crm.models.crm_lead import Lead
 from odoo.addons.fastapi.dependencies import odoo_env
-from ..schemas import Lead
+from ..schemas import (
+    Lead,
+    LeadFormSchema
+)
 
 
-router = APIRouter(tags=['no_demo'])
+router = APIRouter(tags=['crm_lead'])
 
 
-@router.post('/crm_lead/create/')
-async def create_lead(lead: Lead, env: Annotated[
-        Environment, Depends(odoo_env)]) -> Lead:
-    data = lead.model_dump(by_alias=True)
+@router.post('/crm_lead/{form_name}/')
+async def create_lead(
+        form_name: str,
+        lead_form: LeadFormSchema,
+        env: Annotated[Environment, Depends(odoo_env)],
+    ):
+    lead_form_data = lead_form.model_dump(by_alias=True)
     crm_lead = env['crm.lead']
-    crm_lead.sudo().create(data)
-    return data
+    stepone_form = env['stepone.form']
+    try:
+        with env.cr.savepoint():    
+            lead_obj = crm_lead.sudo().create(lead_form_data['lead'])
+            form_data = lead_form_data['form']
+            form_data['lead_id'] = lead_obj.id
+            stepone_form.sudo().create(form_data)
+    except Exception as e:
+        error_messages = 'Something went wrong'
+        raise HTTPException(status_code=422, detail=error_messages)
+    return {'message': 'OK!'}
