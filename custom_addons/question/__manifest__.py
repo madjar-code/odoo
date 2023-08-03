6257eb56808bcce06def66b8dd84091140ca38b4
Evan Madjar <evan.madjar@datadrivemd.com>
2023-08-03 09:46:23 +0300

    feat: Init new app for question form

---
+++ custom_addons/question/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'question data',
+    'summary': """website with question form""",
+    'author': 'Evan Madjar',
+    'depends': (
+        'base',
+    ),
+    'version': '0.1',
+    'installable': True,
+    'application': True,
+}
