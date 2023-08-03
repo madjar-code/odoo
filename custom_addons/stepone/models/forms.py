1e630944c396ce390c5e9f599a0cdcea0302f51f
Evan Madjar <evan.madjar@datadrivemd.com>
2023-08-03 16:25:18 +0300

    feat: Update stepone models with enumerates
    Enumerates:
    - TRANSPORT_CAR_TYPES;
    - TRANSPORTATION_TYPES;
    - EQUIPMENT_TYPES;
    - VEHICLE_TYPES;
    - RATE_CHOICES.

--- custom_addons/stepone/models/forms.py
+++ custom_addons/stepone/models/forms.py
@@ -38,6 +38,17 @@ class DefaultContactForm(models.Model):
 class InstantForm(models.Model):
     _name = 'instant.form'
     _description = 'Instant Form Model'
+    
+    TRANSPORT_CAR_TYPES = (
+        ('Car', 'Car'),
+        ('SUV', 'SUV'),
+        ('Pickup', 'Pickup'),
+        ('Van', 'Van'),
+        ('RV', 'RV'),
+        ('Moto', 'Moto'),
+        ('Heavy Equipment', 'Heavy Equipment'),
+        ('General Freight', 'General Freight'),
+    )
 
     lead_id = fields.Many2one('crm.lead',
                               inverse_name='instant_form_id',
@@ -45,7 +56,8 @@ class InstantForm(models.Model):
                               ondelete='cascade')
     transport_car_from = fields.Char()
     transport_car_to = fields.Char()
-    transport_car_type = fields.Char()
+    transport_car_type = fields.Selection(selection=TRANSPORT_CAR_TYPES,
+                                          string='Transport Car Types')
 
     _sql_constraints = (
         ('lead_id_unique', 'UNIQUE(lead_id)',
@@ -74,14 +86,39 @@ class RequestQuoteForm(models.Model):
     _name = 'request.quote.form'
     _description = 'Request Quote Form Model'
 
+    TRANSPORTATION_TYPES = (
+        ('Door-to-door', 'Door-to-door'),
+        ('Terminal-to-terminal', 'Terminal-to-terminal'),
+    )
+    EQUIPMENT_TYPES = (
+        ('Open Car Hauler', 'Open Car Hauler'),
+        ('Enclosed Car Hauler', 'Enclosed Car Hauler'),
+        ('General or Large Freight', 'General or Large Freight'),
+    )
+    VEHICLE_TYPES = (
+        ('Car', 'Car'),
+        ('SUV', 'SUV'),
+        ('Pickup', 'Pickup'),
+        ('Van', 'Van'),
+        ('Moto', 'Moto'),
+        ('ATV', 'ATV'),
+        ('RV', 'RV'),
+        ('Heavy Equipment', 'Heavy Equipment'),
+        ('Large Yacht', 'Large Yacht'),
+        ('Travel Trailer', 'Travel Trailer'),
+    )
+
     lead_id = fields.Many2one('crm.lead',
                               inverse_name='request_quote_form_id',
                               string='Related Lead',
                               ondelete='cascade')
-    transportation_type = fields.Char()
-    equipment_type = fields.Char()
-    vehicle_type = fields.Char()
-    note_type = fields.Char()
+
+    transportation_type = fields.Selection(selection=TRANSPORTATION_TYPES,
+                                           string='Transportation Type')
+    equipment_type = fields.Selection(selection=EQUIPMENT_TYPES,
+                                      string='Equipment Type')
+    vehicle_type = fields.Selection(selection=VEHICLE_TYPES,
+                                    string='Vehicle Type')
     note = fields.Text()
 
     _sql_constraints = (
@@ -93,12 +130,26 @@ class RequestQuoteForm(models.Model):
 class ReviewForm(models.Model):
     _name = 'review.form'
     _description = 'Review Form Model'
+    
+    RATE_CHOICES = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+        ('6', '6'),
+        ('7', '7'),
+        ('8', '8'),
+        ('9', '9'),
+        ('10', '10'),
+    )
 
     lead_id = fields.Many2one('crm.lead',
                               inverse_name='review_form_id',
                               string='Related Lead',
                               ondelete='cascade')
-    rate = fields.Char()
+    rate = fields.Selection(
+        selection=RATE_CHOICES, string='Rate', required=True,)
 
     _sql_constraints = (
         ('lead_id_unique', 'UNIQUE(lead_id)',
