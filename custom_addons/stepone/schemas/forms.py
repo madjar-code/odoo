1d9dc561c7deaa6cb4db677feedb085f7c7419e8
Evan Madjar <evan.madjar@datadrivemd.com>
2023-08-04 09:51:28 +0300

    refactor: Remove enum classes from schemas.py

--- custom_addons/stepone/schemas/forms.py
+++ custom_addons/stepone/schemas/forms.py
@@ -1,10 +1,8 @@
-from enum import Enum
-from typing import ClassVar
+from ..choices import *
 from pydantic import (
     BaseModel,
     HttpUrl,
     constr,
-    validator,
 )
 
 
@@ -16,17 +14,6 @@ class SteponeForm(BaseModel):
 
 class DefaultContactForm(BaseModel):
     message: str
-
-
-class TransportCarType(str, Enum):
-    car = 'Car'
-    suv = 'SUV'
-    pickup = 'Pickup'
-    van = 'Van'
-    moto = 'Moto'
-    rv = 'RV'
-    heavy_equipment = 'Heavy Equipment'
-    general_freight = 'General Freight'
 
 
 class InstantForm(BaseModel):
@@ -43,30 +30,6 @@ class QuestionForm(BaseModel):
     message: str
 
 
-class TransportationType(str, Enum):
-    D2D = 'Door-to-door'
-    T2T = 'Terminal-to-terminal'
-
-
-class EquipmentType(str, Enum):
-    open_car_hauler = 'Open Car Hauler'
-    enclosed_car_hauler = 'Enclosed Car Hauler'
-    general_or_large_freight = 'General or Large Freight'
-
-
-class VehicleType(str, Enum):
-    car = 'Car'
-    suv = 'SUV'
-    pickup = 'Pickup'
-    van = 'Van'
-    moto = 'Moto'
-    atv = 'ATV'
-    rv = 'RV'
-    heavy_equipment = 'Heavy Equipment'
-    large_yacht = 'Large Yacht'
-    travel_trailer = 'Travel Trailer'
-
-
 class RequestQuoteForm(BaseModel):
     transportation_type: TransportationType
     equipment_type: EquipmentType
@@ -75,19 +38,6 @@ class RequestQuoteForm(BaseModel):
 
     class Config:
        use_enum_values = True
-
-
-class RateChoice(str, Enum):
-    one = '1'
-    two = '2'
-    three = '3'
-    four = '4'
-    five = '5'
-    six = '6'
-    seven = '7'
-    eight = '8'
-    nine = '9'
-    ten = '10'
 
 
 class ReviewForm(BaseModel):
