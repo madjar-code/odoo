e60161199709f54623a9e8e788242b753c1cd85c
Evan Madjar <evan.madjar@datadrivemd.com>
2023-08-04 10:54:02 +0300

    feat: Add exception return from router

--- custom_addons/crm_api/routers/routers.py
+++ custom_addons/crm_api/routers/routers.py
@@ -1,8 +1,9 @@
-from typing import Annotated
+from typing import (
+    Annotated,
+)
 from fastapi import (
     APIRouter,
     Depends,
-    HTTPException,
 )
 from odoo.api import Environment
 from odoo.addons.fastapi.dependencies import odoo_env
@@ -19,18 +20,15 @@ API_PREFIX = '/crm-lead'
 
 router = APIRouter(tags=['crm_lead'])
 
+
 def create_validate_lead_form(lead_form_schema, form_table, env):
     lead_form_data = lead_form_schema.model_dump(by_alias=True)
     crm_lead = env['crm.lead']
-    try:
-        with env.cr.savepoint():
-            lead_obj = crm_lead.sudo().create(lead_form_data['lead'])
-            form_data = lead_form_data['form']
-            form_data['lead_id'] = lead_obj.id
-            form_table.sudo().create(form_data)
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=422, detail='Something went wrong')
+    with env.cr.savepoint():
+        lead_obj = crm_lead.sudo().create(lead_form_data['lead'])
+        form_data = lead_form_data['form']
+        form_data['lead_id'] = lead_obj.id
+        form_table.sudo().create(form_data)
     return {'message': 'OK!'}
 
 
