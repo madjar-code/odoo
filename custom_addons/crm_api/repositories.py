ca85446918cdac4469b21f5a1baf735315c956e8
Evan Madjar <evan.madjar@datadrivemd.com>
2023-08-07 14:02:47 +0300

    refactor: Add an repository interface

--- custom_addons/crm_api/repositories.py
+++ custom_addons/crm_api/repositories.py
@@ -1,8 +1,21 @@
+import psycopg2
+from abc import ABC, abstractmethod
 from odoo.api import Environment
 from pydantic import BaseModel
 
 
-class OdooLeadFormRepository:
+class AbstractRepository(ABC):
+    @abstractmethod
+    def __init__(self, lead_table_name: str) -> None:
+        """Save the lead_table_name value."""
+
+    @abstractmethod
+    def create_validate_lead_form(self, lead_form_schema: BaseModel,
+                                  form_table_name: str, env: Environment) -> None:
+        """Validate and create lead and form by schema."""
+
+
+class OdooLeadFormRepository(AbstractRepository):
     def __init__(self, lead_table_name: str) -> None:
         self.lead_table_name = lead_table_name
 
@@ -17,3 +30,36 @@ class OdooLeadFormRepository:
             form_data = lead_form_data['form']
             form_data['lead_id'] = lead_obj.id
             form_table.sudo().create(form_data)
+
+
+# class SQLLeadFormRepository(AbstractRepository):
+#     def __init__(self, lead_table_name: str) -> None:
+#         self.lead_table_name = lead_table_name.replace('.', '_')
+
+#     def create_validate_lead_form(self, lead_form_schema: BaseModel,
+#                                   form_table_name: str, env: Environment) -> None:
+#         lead_form_data = lead_form_schema.dict()
+#         lead_data = lead_form_data['lead']
+#         form_data = lead_form_data['form']
+#         form_table_name = form_table_name.replace('.', '_')
+
+#         connection = psycopg2.connect(
+#             database='',
+#             user='',
+#             password='',
+#             host='',
+#             port='',
+#         )
+        
+#         with connection.cursor() as cursor:
+#             lead_columns = ', '.join(lead_data.keys())
+#             lead_values = ', '.join(['%s'] * len(lead_data))
+#             lead_query = f'INSERT INTO {self.lead_table_name} ({lead_columns}) VALUES ({lead_values})'
+#             cursor.execute(lead_query, tuple(lead_data.value()))
+#             lead_id = cursor.fetchone()[0]
+
+#             form_data['lead_id'] = lead_id
+#             form_columns = ', '.join(form_data.keys())
+#             form_values = ', '.join(['%s'] * len(form_data))
+#             form_query = f'INSERT INTO {form_table_name} ({form_columns}) VALUES ({form_values})'
+#             cursor.execute(form_query, tuple(form_data.values()))
