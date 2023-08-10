b19c819a7c257b3ec356b099e55f99bd778adcd1
Evan Madjar <evan.madjar@datadrivemd.com>
2023-08-10 10:38:42 +0300

    feat: Init custom enrich_data module
    This custom addon is iap_enrich_api analog

---
+++ custom_addons/enrich_data/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Data Enricher',
+    'summary': """Custom API for enrich data by contact""",
+    'author': 'Evan Madjar',
+    'external_dependencies': {
+        'python': (
+            'pydantic',
+            'requests',
+            'bs4',
+        )
+    },
+    'installable': True,
+    'application': True,
+}
