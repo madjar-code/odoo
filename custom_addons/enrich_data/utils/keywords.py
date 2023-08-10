794498b5e5ddcd66f1ffe90040b29a91401b873d
Evan Madjar <evan.madjar@datadrivemd.com>
2023-08-10 11:34:53 +0300

    chore: Add file with key constants
    This file has constants for extracting data from
    website

---
+++ custom_addons/enrich_data/utils/keywords.py
@@ -0,0 +1,28 @@
+# strings to searching for social websites
+SOCIAL_NETWORKS = (
+    'facebook',
+    'twitter',
+    'instagram',
+    'linkedin',
+    'youtube',
+)
+
+# strings to searching for URL of contact page
+CONTACT_STRINGS = (
+    'contact',
+    'contact',
+    'контакт',
+)
+
+# keywords to searching for address
+ADDRESS_KEYWORDS = (
+    'str.',
+    'street',
+    'highway',
+    'rd',
+    'road',
+    'united states',
+    'moldova',
+    'шос.',
+    'обл.',
+)
