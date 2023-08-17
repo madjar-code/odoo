268ba90222924b9403e04cdbfd502ed18907139a
Evan Madjar <evan.madjar@datadrivemd.com>
2023-08-17 09:34:40 +0300

    feat: Remove form tables from DB

--- custom_addons/stepone/models/forms.py
+++ custom_addons/stepone/models/forms.py
@@ -2,88 +2,88 @@ from ..choices import *
 from odoo import models, fields
 
 
-class AbstractForm(models.AbstractModel):
-    _name = 'abstract.form'
-    _description = 'Abstact form with related-field'
+# class AbstractForm(models.AbstractModel):
+#     _name = 'abstract.form'
+#     _description = 'Abstact form with related-field'
 
-    lead_id = fields.Many2one('crm.lead',
-                              string='Related Lead',
-                              ondelete='cascade')
-    _sql_constraints = (
-        ('lead_id_unique', 'UNIQUE(lead_id)',
-         'Each Form can be linked to only one CRM Lead.'),
-    )
-
-
-class SteponeForm(models.Model):
-    _name = 'stepone.form'
-    _inherit = 'abstract.form'
-
-    linkedIn = fields.Char(string='LinkedIn URL')
-    file = fields.Char(string='File URL')
-    introduction = fields.Text(string='Introduction')
+#     lead_id = fields.Many2one('crm.lead',
+#                               string='Related Lead',
+#                               ondelete='cascade')
+#     _sql_constraints = (
+#         ('lead_id_unique', 'UNIQUE(lead_id)',
+#          'Each Form can be linked to only one CRM Lead.'),
+#     )
 
 
-class DefaultContactForm(models.Model):
-    _name = 'default.form'
-    _inherit = 'abstract.form'
-    _description = 'Default Contact Form Model'
+# class SteponeForm(models.Model):
+#     _name = 'stepone.form'
+#     _inherit = 'abstract.form'
 
-    message = fields.Text()
-
-
-class InstantForm(models.Model):
-    _name = 'instant.form'
-    _inherit = 'abstract.form'
-    _description = 'Instant Form Model'
-
-    transport_car_from = fields.Char()
-    transport_car_to = fields.Char()
-    transport_car_type = fields.Selection(
-        selection=[(item.value, item.name)
-                   for item in TransportCarType],
-        string='Transport Car Types')
+#     linkedIn = fields.Char(string='LinkedIn URL')
+#     file = fields.Char(string='File URL')
+#     introduction = fields.Text(string='Introduction')
 
 
-class QuestionForm(models.Model):
-    _name = 'question.form'
-    _inherit = 'abstract.form'
-    _description = 'Question Form Model'
+# class DefaultContactForm(models.Model):
+#     _name = 'default.form'
+#     _inherit = 'abstract.form'
+#     _description = 'Default Contact Form Model'
 
-    communication_with = fields.Char(string='With whom to communicate')
-    message = fields.Text(string='Message to Server')
-
-
-class RequestQuoteForm(models.Model):
-    _name = 'request.quote.form'
-    _inherit = 'abstract.form'
-    _description = 'Request Quote Form Model'
-
-    transportation_type = fields.Selection(
-        selection=[(item.value, item.name)
-                   for item in TransportationType],
-        string='Transportation Type'
-    )
-    equipment_type = fields.Selection(
-        selection=[(item.value, item.name)
-                   for item in EquipmentType],
-        string='Equipment Type'
-    )
-    vehicle_type = fields.Selection(
-        selection=[(item.value, item.name)
-                   for item in VehicleType],
-        string='Vehicle Type'
-    )
-    note = fields.Text()
+#     message = fields.Text()
 
 
-class ReviewForm(models.Model):
-    _name = 'review.form'
-    _inherit = 'abstract.form'
-    _description = 'Review Form Model'
+# class InstantForm(models.Model):
+#     _name = 'instant.form'
+#     _inherit = 'abstract.form'
+#     _description = 'Instant Form Model'
 
-    rate = fields.Selection(
-        selection=[(item.value, item.name)
-                   for item in RateChoice],
-        string='Rate', required=True,
-    )
+#     transport_car_from = fields.Char()
+#     transport_car_to = fields.Char()
+#     transport_car_type = fields.Selection(
+#         selection=[(item.value, item.name)
+#                    for item in TransportCarType],
+#         string='Transport Car Types')
+
+
+# class QuestionForm(models.Model):
+#     _name = 'question.form'
+#     _inherit = 'abstract.form'
+#     _description = 'Question Form Model'
+
+#     communication_with = fields.Char(string='With whom to communicate')
+#     message = fields.Text(string='Message to Server')
+
+
+# class RequestQuoteForm(models.Model):
+#     _name = 'request.quote.form'
+#     _inherit = 'abstract.form'
+#     _description = 'Request Quote Form Model'
+
+#     transportation_type = fields.Selection(
+#         selection=[(item.value, item.name)
+#                    for item in TransportationType],
+#         string='Transportation Type'
+#     )
+#     equipment_type = fields.Selection(
+#         selection=[(item.value, item.name)
+#                    for item in EquipmentType],
+#         string='Equipment Type'
+#     )
+#     vehicle_type = fields.Selection(
+#         selection=[(item.value, item.name)
+#                    for item in VehicleType],
+#         string='Vehicle Type'
+#     )
+#     note = fields.Text()
+
+
+# class ReviewForm(models.Model):
+#     _name = 'review.form'
+#     _inherit = 'abstract.form'
+#     _description = 'Review Form Model'
+
+#     rate = fields.Selection(
+#         selection=[(item.value, item.name)
+#                    for item in RateChoice],
+#         string='Rate', required=True,
+#     )
