11f613251b8c77cee92e55a849d6b8d55500a0b1
Evan Madjar <evan.madjar@datadrivemd.com>
2023-08-22 13:18:24 +0300

    feat: Update stepone-form endpoint with file load

--- custom_addons/crm_api/routers/routers.py
+++ custom_addons/crm_api/routers/routers.py
@@ -1,13 +1,21 @@
+import os
+import uuid
+import shutil
 from typing import (
     Annotated,
 )
 from fastapi import (
     APIRouter,
     Depends,
+    File,
+    UploadFile,
+    HTTPException,
 )
 from odoo.api import Environment
 from odoo.addons.fastapi.dependencies import odoo_env
+from ...stepone.schemas.forms import SteponeForm
 from ..schemas import (
+    LeadSchema,
     LeadSteponeFormSchema,
     LeadQuestionFormSchema,
     LeadInstantFormSchema,
@@ -28,16 +36,55 @@ lead_form_repo = OdooLeadFormRepository('crm.lead')
 
 @router.post(f'{API_PREFIX}/stepone-form/')
 async def create_lead_stepone(
-        lead_form: LeadSteponeFormSchema,
         env: Annotated[Environment, Depends(odoo_env)],
+        lead: LeadSchema = Depends(),
+        form: SteponeForm = Depends(),
+        file: UploadFile = File(...),
     ):
+    unique_dir_name = str(uuid.uuid4())
+    upload_dir = os.path.join(os.getcwd(), f'uploads/{unique_dir_name}')
+
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+        
+    dest = os.path.join(upload_dir, file.filename)
+
+    with open(dest, 'wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    DIR_PREFIX = 'http://localhost:8069/uploads'
+    ALLOWED_FORMATS = (
+        '.doc',
+        '.docx',
+        'application/msword',
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        '.pdf',
+    )
+
+    _, file_format = os.path.splitext(file.filename)
+    
+    if file_format not in ALLOWED_FORMATS:
+        raise HTTPException(status_code=400, detail='Incorrect file format')
+
+    file_size = file.file.tell()
+    
+    TOP_SIZE = 20 * 1024 * 1024  # 20 MB
+    if file_size > TOP_SIZE:
+        raise HTTPException(status_code=400, detail='File is too big')
+
+    form.file = f'{DIR_PREFIX}/{unique_dir_name}/{file.filename}'
+    lead_form_data = {
+        'lead': lead.model_dump(),
+        'form': form.model_dump(),
+    }
+    lead_form = LeadSteponeFormSchema(**lead_form_data)
     lead_form_repo.create_validate_lead_form(lead_form, env)
     return {'message': 'stepone form was created!'}
 
 
 @router.post(f'{API_PREFIX}/question-form/')
 async def create_lead_question(
-        lead_form: LeadQuestionFormSchema,
+        lead_form: LeadQuestionFormSchema ,
         env: Annotated[Environment, Depends(odoo_env)],
     ):
     lead_form_repo.create_validate_lead_form(lead_form, env)
