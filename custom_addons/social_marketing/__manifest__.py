751950b7dc6b4da8e0ca6da7917d723d1b2dd110
Evan Madjar <evan.madjar@datadrivemd.com>
2023-09-06 16:24:43 +0300

    Initialize a new module named social_marketing

---
+++ custom_addons/social_marketing/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Social Marketing',
+    'summary': 'Use it for social marketing purposes',
+    'category': 'Sales',
+    'version': '0.1',
+    # 'depends': (
+    # ),|
+    # 'data': (
+    #     'data/ir_action.xml',
+    #     'data/mail_templates.xml',
+    # ),
+    'installable': True,
+    'application': True,
+}
