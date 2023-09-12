016952dc46b41690a3de90f5d1fd9f2b840c6772
Evan Madjar <evan.madjar@datadrivemd.com>
2023-09-12 14:07:49 +0300

    feat: Create models for store accounts

---
+++ custom_addons/social_marketing/models/accounts.py
@@ -0,0 +1,38 @@
+from odoo import (
+    models,
+    fields,
+)
+from ..custom_types import SocialMediaType
+
+
+class Accounts(models.Model):
+    _name = 'marketing.accounts'
+
+    social_media = fields.Selection(
+        selection=[(item.value, item.name)
+                   for item in SocialMediaType],
+        string='Account Social Media')
+    id_name = fields.Char('ID name for account identifying', required=False)
+    full_name = fields.Char('This is full account name')
+
+    post_ids = fields.One2many('marketing.posts','account_id',
+                                string='Related Account Posts')
+    
+    fb_credentials_id = fields.Many2one('marketing.fb.credentials',
+                                        string='Related Credentials Data',
+                                        ondelete='cascade', required=False)
+    _sql_constraints = (
+        ('fb_credentials_id_unique', 'UNIQUE(fb_credentials_id)',
+         'Each Account can be linked to only one Facebook credentials.'),
+    )
+
+
+class FacebookCredentials(models.Model):
+    _name = 'marketing.fb.credentials'
+
+    access_token = fields.Char(string='Facebook Access Token')
+    page_id = fields.Char(string='Facebook Page ID')
+    
+    account_id = fields.One2many('marketing.accounts',
+                                 'fb_credentials_id',
+                                 string='Related Account')
