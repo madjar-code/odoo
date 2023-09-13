e04345c71eea753ee31f28a279997ca3e184dc34
Evan Madjar <evan.madjar@datadrivemd.com>
2023-09-13 16:24:41 +0300

    chore: Add some custom types for social_marketing

--- custom_addons/social_marketing/custom_types.py
+++ custom_addons/social_marketing/custom_types.py
@@ -13,12 +13,20 @@ FieldName: TypeAlias = str
 IdType: TypeAlias = int
 ErrorType: TypeAlias = str
 PostsListType: TypeAlias = List[Dict]
+PostType: TypeAlias = Dict
+CommentsListType: TypeAlias = List[Dict]
+CommentType: TypeAlias = Dict
 FieldErrorsType: TypeAlias = Dict[FieldName, List[str]]
-
 
 # class PostType(str, Enum):
 #     common = 'common'
 #     single = 'single'
+
+
+class Message(NamedTuple):
+    success: bool
+    field_name: FieldName
+    text: str
 
 
 class PostState(str, Enum):
@@ -37,18 +45,22 @@ class SocialMediaType(str, Enum):
 
 
 class PostObject(NamedTuple):
-    social_id: IdType
+    social_id: Optional[IdType]
     reposts_qty: Optional[int]
     likes_qty: Optional[int]
     views_qty: Optional[int]
     comments_qty: Optional[int]
     message: Optional[str]
     state: Optional[PostState]
+    account_id: Optional[IdType]
+    # account_id_name: Optional[IdType]
+    # schedule_time: Optional[str]
     # type: Optional[PostType]
     # image_urls: List[AnyUrl]
 
 
 class AccountObject(NamedTuple):
-    id_name: str
+    id: IdType
+    id_name: Optional[IdType]
     social_media: SocialMediaType
     credentials: Dict[str, str]
