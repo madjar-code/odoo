42d83c2db1d1f46fb6f55ad24c055caf15288202
Evan Madjar <evan.madjar@datadrivemd.com>
2023-10-03 12:00:07 +0300

    chore: Temporary connector update

--- custom_addons/social_marketing/utils/post/connectors.py
+++ custom_addons/social_marketing/utils/post/connectors.py
@@ -1,29 +1,27 @@
 import json
 import requests
+from typing import (
+    List,
+)
 from abc import (
     ABC,
     abstractmethod,
 )
 from ...custom_types import (
     ImageType,
-    ImageObject,
     PostObject,
     IdType,
     PostType,
-    CommentType,
 )
 from ...custom_exceptions import (
     RequestException,
 )
+import facebook
 
 
 class ConnectorInterface(ABC):
     @abstractmethod
     def create_post(self, post_data: PostType) -> PostType:
-        pass
-
-    @abstractmethod
-    def create_comment_on_post(self, post_id: IdType) -> CommentType:
         pass
 
     @abstractmethod
@@ -33,124 +31,81 @@ class ConnectorInterface(ABC):
 
 GPAPH_API_VERSION = 'v17.0'
 
+
 class FacebookConnector(ConnectorInterface):
     API_PREFIX = f'https://graph.facebook.com/{GPAPH_API_VERSION}'
 
-    def __init__(self, page_id: IdType, access_token: str,
-                 account_id: IdType, post_object: PostObject = None) -> None:
+    def __init__(self, page_id: IdType, access_token: str, account_id: IdType, post_object: PostObject = None) -> None:
         self._page_id = page_id
         self._access_token = access_token
         self.account_id = account_id
         self._post_object = post_object
 
     def create_post(self, post_data: PostType) -> PostType:
-        """
-        Can be used for:
-        - scheduled post;
-        - post with link;
-        - post only with text;
-        """
         url = f'{self.API_PREFIX}/{self._page_id}/feed'
         params = post_data
+        print(f'\n\n{params}\n\n')
         params['access_token'] = self._access_token
         response = requests.post(url=url, params=params)
         response_data = json.loads(response.text)
+        print(f'\n\n{response_data}')
         if response_data.get('error'):
             raise RequestException(response_data['error']['message'])
         return response_data
 
-    def create_post_with_one_image(self, post_data: PostType,
-                                   image_data: ImageType) -> PostType:
+    def upload_image_to_facebook(self, image_data: ImageType) -> IdType:
         url = f'{self.API_PREFIX}/{self._page_id}/photos/'
-        params = post_data
-        params['access_token'] = self._access_token
+        params = {
+            'access_token': self._access_token,
+        }
         response = requests.post(url=url, params=params, files=image_data)
         response_data = json.loads(response.text)
         if response_data.get('error'):
             raise RequestException(response_data['error']['message'])
-        return response_data
+        return response_data.get('id')
 
-    # def create_image_on_post(self, post_id: IdType,
-    #                          image_data: ImageType) -> ImageType:
-    #     url = f'{self.API_PREFIX}/{post_id}/photos/'
-    #     params = image_data
-    #     params['access_token'] = self._access_token
-    #     response = requests.post(url=url, params=params, files=image_data)
-    #     response_data = json.loads(response.text)
-    #     if response_data.get('error'):
-    #         raise RequestException(response_data['error']['message'])
-    #     return response_data
-
-    def update_post_state(self, post_id: IdType) -> PostType:
-        url = f'{self.API_PREFIX}/{post_id}/'
-        params = {
-            'published': True,
+    def flex_create_post_test(self) -> PostType:
+        graph = facebook.GraphAPI(access_token=self._access_token)
+        image_ids = ['122099841260055468', '122099842838055468']
+        post = {
+            'message': '123456',
+            'attached_media': [{'media_fbid': image_id} for image_id in image_ids]
         }
-        params['access_token'] = self._access_token
-        response = requests.post(url=url, params=params)
-        response_data = json.loads(response.text)
-        if response_data.get('error'):
-            raise RequestException(response_data['error']['message'])
-        return response_data
-
-    def create_comment_on_post(
-            self, post_id: IdType, comment_data: CommentType) -> CommentType:
-        url = f'{self.API_PREFIX}/{post_id}/comments/'
-        params = comment_data
-        params['access_token'] = self._access_token
-        response = requests.post(url=url, params=params)
-        response_data = json.loads(response.text)
-        if response_data.get('error'):
-            raise RequestException(response_data['error']['message'])
-        return response_data
+        graph.put_object(parent_object='me', connection_name='feed', **post)
 
     def flex_create_post(self) -> PostType:
-        post_data = self._post_object
+        post_object = self._post_object
         image_objects = self._post_object.image_objects
+        media_ids: List[IdType] = []
+        
+        graph = facebook.GraphAPI(access_token=self._access_token)
+        # for image_object in image_objects:
+        #     response = graph.put_photo(image=image_object.image)
+        #     media_id = response['id']
+        #     media_ids.append({'photoId': str(media_id)})
 
-        if image_objects and len(image_objects) == 1:
-            image_object: ImageObject = image_objects[0]
-            response_data = self.create_post_with_one_image(
-                {
-                    'message': post_data.message if post_data.message else '',
-                },
-                {
-                    'source': (
-                        f'{image_object.name}',
-                        image_object.image,
-                        f'image/{image_object.format}',
-                    )
-                },
-            )
-        # elif image_objects:
-        #     image_data_list = []
+        # media_ids = json.dumps(media_ids)
+
+        # if image_objects:
         #     for image_object in image_objects:
-        #         image_data = (
-        #             f'{image_object.name}',
-        #             image_object.image,
-        #             f'image/{image_object.format}',   
-        #         )
-        #         image_data_list.append(image_data)
-        #     response_data = self.create_post_with_multiple_images(
-        #         {
-        #             'message': post_data.message,
-        #         },
-        #         image_data_list,
-        #     )
-
-        # elif post_data.schedule_time:
-        #     response_data = self.create_post(
-        #         {
-        #             'message': post_data.schedule_time,
-        #             'published': 'false',
-        #             'scheduled_publishing_time': post_data.schedule_time,
-        #         }
-        #     )
-
-        elif post_data:
-            response_data = self.create_post(
-                {
-                    'message': str(post_data.message),
-                }
-            )
+        #         media_id = self.upload_image_to_facebook({
+        #             'source': (
+        #                 f'{image_object.name}',
+        #                 image_object.image,
+        #                 f'image/{image_object.format}',
+        #             )
+        #         })
+        #         media_ids.append(media_id)
+        if media_ids:
+            attached_media = [{'media_fbid': media_id} for media_id in media_ids]
+            response_data = self.create_post({
+                'message': post_object.message if post_object.message else '',
+                'attached_media': attached_media,
+            })
+        else:
+            response_data = self.create_post({
+                'message': post_object.message if post_object.message else '',
+                'attached_media': []
+            })
+            
         return response_data
