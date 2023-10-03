c9204ecf6de661bdec719971b5f4652c9cec76c4
Evan Madjar <evan.madjar@datadrivemd.com>
2023-10-03 12:00:49 +0300

    feat: Add several actions to post model
    The following actions were added:
    - cancel action;
    - create action;
    - update action.

--- custom_addons/social_marketing/models/posts.py
+++ custom_addons/social_marketing/models/posts.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 from odoo import (
     models,
     fields,
     api,
 )
+from odoo.exceptions import ValidationError
 from typing import (
     List,
     Dict,
@@ -11,9 +13,10 @@ from ..utils.data_sync_2 import (
     DataSynchronizer2,
 )
 from ..custom_types import (
-    IdType,
+    PostObject,
     PostState,
     AccountObject,
+    ImageObject,
 )
 
 
@@ -35,7 +38,6 @@ class PostsWrapper(models.Model):
     
     @api.depends('post_ids.likes_qty')
     def _compute_total_likes(self) -> int:
-        print(f'\n\SMTH Happends\n\n')
         for wrapper in self:
             total_likes = sum(wrapper.post_ids.mapped('likes_qty'))
             wrapper.total_likes = total_likes
@@ -49,14 +51,23 @@ class SocialPosts(models.Model):
     _name = 'marketing.posts'
 
     social_id = fields.Char(
-        string='Post ID in Social Media', required=False)
+        string='Post ID in Social Media',
+        required=False, readonly=True)
     message = fields.Char(string='Post Message', required=False)
 
     schedule_time = fields.Datetime(
         string='Schedule Time Field', required=False)
+
+    posted_time = fields.Datetime(
+        string='Posted Time',
+        # compute='_compute_posted_time',
+        store=True,
+        readonly=True,
+    )
     state = fields.Selection(
         selection=[(item.value, item.name)
                     for item in PostState],
+        readonly=True,
         string='Post State', default=PostState.draft.value)
 
     likes_qty = fields.Integer(
@@ -68,13 +79,14 @@ class SocialPosts(models.Model):
     comments_qty = fields.Integer(
         string='Number of Comments', default=0, readonly=True)
     
-    changed = fields.Boolean(
-        string='Is Synced?', default=True, readonly=True)
+    # changed = fields.Boolean(
+    #     string='Is Synced?', default=True, readonly=True)
 
     account_id = fields.Many2one(
         'marketing.accounts',
         string='Related Account',
         ondelete='cascade',
+        required=True,
     )
     wrapper_id = fields.Many2one(
         'marketing.posts.wrapper',
@@ -89,7 +101,68 @@ class SocialPosts(models.Model):
         required=False,
     )
 
-    def _get_all_account_objects(self) -> None:
+    @api.onchange('schedule_time')
+    def _onchange_schedule_time(self):
+        for post in self:
+            if post.schedule_time and datetime.now() > post.schedule_time:
+                raise ValidationError('Schedule time cannot be in the past.')
+    
+    def action_cancel(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'marketing.posts',
+            'view_mode': 'tree,form',
+            'view_type': 'form',
+            'target': 'current',
+            '_rec_name': 'hui slona',
+        }
+
+    def action_finish_later(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'marketing.posts',
+            'view_mode': 'form',
+            'view_type': 'form',
+            'res_id': self.id,
+            'target': 'current',
+        }
+
+    def action_update_post(self):
+        if self.social_id:
+            self._update_post()
+        else:
+            pass
+
+    def _update_post(self):
+        print('\n\nАпдейт поста\n\n')
+
+    def action_create_post(self):
+        if not self.social_id:
+            self._create_post()
+        else:
+            pass
+
+    def _get_one_account_for_post(self) -> List[AccountObject]:
+        acc = self.account_id
+        if acc.social_media == 'Facebook':
+            credentials: Dict[str, str] = {
+                'access_token': acc.fb_credentials_id.access_token,
+                'page_id': acc.fb_credentials_id.page_id,
+            }
+        account_object = AccountObject(
+            acc.id, None, acc.social_media, credentials
+        )
+        return [account_object]
+
+    def _create_post(self) -> None:
+        account_list_one_post = self._get_one_account_for_post()
+        DataSynchronizer2(['Facebook'], account_list_one_post,
+                        self.env['marketing.posts'],
+                        self.env['marketing.image'],
+                        self.env['marketing.accounts'],
+                        ).create_one_post_from_db(self)
+
+    def _get_all_account_objects(self) -> List[AccountObject]:
         accounts = self.env['marketing.accounts'].search([])
         account_list: List[AccountObject] = []
         for acc in accounts:
@@ -118,7 +191,10 @@ class SocialPosts(models.Model):
                          self.env['marketing.accounts'],
                          ).from_db_to_accounts()
 
-    # def get_all_social_ids(self) -> List[IdType]:
-    #     posts = self.env['marketing.posts'].search_read([], ['social_id'])
-    #     social_ids = [post['social_id'] for post in posts]
-    #     return social_ids
+    def write(self, values: Dict[str, str]):
+        if 'schedule_time' in values:
+            new_schedule_time_str = values.get('schedule_time')
+            new_schedule_time = fields.Datetime.from_string(new_schedule_time_str)
+            if new_schedule_time and new_schedule_time < fields.Datetime.now():
+                raise ValidationError('Schedule time is in the past')
+        return super(SocialPosts, self).write(values)
