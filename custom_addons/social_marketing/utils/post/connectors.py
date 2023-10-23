45500a1be1ef9275b78534975f712701b5f129fb
Evan Madjar <evan.madjar@datadrivemd.com>
2023-10-23 13:42:50 +0300

    feat: Add API func-ty to create post with images

--- custom_addons/social_marketing/utils/post/connectors.py
+++ custom_addons/social_marketing/utils/post/connectors.py
@@ -44,11 +44,9 @@ class FacebookConnector(ConnectorInterface):
     def create_post(self, post_data: PostType) -> PostType:
         url = f'{self.API_PREFIX}/{self._page_id}/feed'
         params = post_data
-        print(f'\n\n{params}\n\n')
         params['access_token'] = self._access_token
         response = requests.post(url=url, params=params)
         response_data = json.loads(response.text)
-        print(f'\n\n{response_data}')
         if response_data.get('error'):
             raise RequestException(response_data['error']['message'])
         return response_data
@@ -57,6 +55,7 @@ class FacebookConnector(ConnectorInterface):
         url = f'{self.API_PREFIX}/{self._page_id}/photos/'
         params = {
             'access_token': self._access_token,
+            'published': False,
         }
         response = requests.post(url=url, params=params, files=image_data)
         response_data = json.loads(response.text)
@@ -64,48 +63,31 @@ class FacebookConnector(ConnectorInterface):
             raise RequestException(response_data['error']['message'])
         return response_data.get('id')
 
-    def flex_create_post_test(self) -> PostType:
-        graph = facebook.GraphAPI(access_token=self._access_token)
-        image_ids = ['122099841260055468', '122099842838055468']
-        post = {
-            'message': '123456',
-            'attached_media': [{'media_fbid': image_id} for image_id in image_ids]
-        }
-        graph.put_object(parent_object='me', connection_name='feed', **post)
-
     def flex_create_post(self) -> PostType:
         post_object = self._post_object
         image_objects = self._post_object.image_objects
         media_ids: List[IdType] = []
-        
-        graph = facebook.GraphAPI(access_token=self._access_token)
-        # for image_object in image_objects:
-        #     response = graph.put_photo(image=image_object.image)
-        #     media_id = response['id']
-        #     media_ids.append({'photoId': str(media_id)})
 
-        # media_ids = json.dumps(media_ids)
+        for image_object in image_objects:
+            media_id = self.upload_image_to_facebook({
+                'source': (
+                    f'{image_object.name}',
+                    image_object.image,
+                    f'image/{image_object.format}',
+                ),
+            })
+            media_ids.append(media_id)
 
-        # if image_objects:
-        #     for image_object in image_objects:
-        #         media_id = self.upload_image_to_facebook({
-        #             'source': (
-        #                 f'{image_object.name}',
-        #                 image_object.image,
-        #                 f'image/{image_object.format}',
-        #             )
-        #         })
-        #         media_ids.append(media_id)
         if media_ids:
             attached_media = [{'media_fbid': media_id} for media_id in media_ids]
             response_data = self.create_post({
                 'message': post_object.message if post_object.message else '',
-                'attached_media': attached_media,
+                'published': True,
+                'attached_media': str(attached_media),
             })
         else:
             response_data = self.create_post({
                 'message': post_object.message if post_object.message else '',
-                'attached_media': []
+                # 'attached_media': []
             })
-            
-        return response_data
+        return response_data, media_ids
