899feb5bc32895cc0b09d85367cec1ab28708370
Evan Madjar <evan.madjar@datadrivemd.com>
2024-01-15 10:55:17 +0200

    feat: Add aggregated_post_id field to Image model

--- custom_addons/social_marketing/models/media.py
+++ custom_addons/social_marketing/models/media.py
@@ -1,7 +1,9 @@
 from odoo import (
+    api,
     models,
     fields,
 )
+from odoo.exceptions import ValidationError
 
 
 class Media(models.AbstractModel):
@@ -26,9 +28,22 @@ class Image(models.Model):
         required=False,
         readonly=True
     )
+    aggregated_post_id = fields.Many2one(
+        'marketing.aggregated.posts',
+        string='Aggregated Post Image',
+        on_delete='cascade',
+        required=False,
+    )
     post_id = fields.Many2one(
         'marketing.posts',
-        string='Image Post',
+        string=' Post Image',
         ondelete='cascade',
         required=False
     )
+
+    # @api.constrains('aggregated_post_id', 'post_id')
+    # def _check_post_ids(self):
+    #     for image_record in self:
+    #         if image_record.aggregated_post_id and\
+    #            image_record.post_id:
+    #             raise ValidationError("Both 'Aggregated Post Image' and 'Post Image' cannot be set simultaneously.")
