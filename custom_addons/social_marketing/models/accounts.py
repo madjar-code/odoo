c190d6880dbc124835aaeeaf330cf002e07f7d74
Evan Madjar <evan.madjar@datadrivemd.com>
2024-01-15 10:08:45 +0200

    feat: Add LinkedIn credentials model

--- custom_addons/social_marketing/models/accounts.py
+++ custom_addons/social_marketing/models/accounts.py
@@ -33,14 +33,23 @@ class Accounts(models.Model):
 
     fb_credentials_id = fields.Many2one(
         'marketing.fb.credentials',
-        string='Related Credentials Data',
+        string='Facebook Related Credentials Data',
+        ondelete='cascade',
+        required=False
+    )
+    li_credentials_id = fields.Many2one(
+        'marketing.li.credentials',
+        string='LinkedIn Related Credentials Data',
         ondelete='cascade',
         required=False
     )
     _sql_constraints = (
         ('fb_credentials_id_unique', 'UNIQUE(fb_credentials_id)',
          'Each Account can be linked to only one Facebook credentials.'),
+        ('li_credentials_id_unique', 'UNIQUE(li_credentials_id)',
+         'Each Account can be linked to only one LinkedIn credentials.'),
     )
+
 
 
 class FacebookCredentials(models.Model):
@@ -49,6 +58,21 @@ class FacebookCredentials(models.Model):
     access_token = fields.Char(string='Facebook Access Token')
     page_id = fields.Char(string='Facebook Page ID')
     
-    account_id = fields.One2many('marketing.accounts',
-                                 'fb_credentials_id',
-                                 string='Related Account')
+    account_id = fields.One2many(
+        'marketing.accounts',
+        'fb_credentials_id',
+        string='Related Account'
+    )
+
+
+class LinkedInCredentials(models.Model):
+    _name = 'marketing.li.credentials'
+
+    access_token = fields.Char(string='LinkedIn Access Token')
+    account_urn = fields.Char(string='LinkedIn Page URN')
+
+    account_id = fields.One2many(
+        'marketing.accounts',
+        'li_credentials_id',
+        string='Related Account'
+    )
